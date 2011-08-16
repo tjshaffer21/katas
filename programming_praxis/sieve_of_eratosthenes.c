956206d49a7b86b73a1312b1950748621fc66fe9
Thomas Shaffer <tjshaffer21@gmail.com>
2011-08-16 17:24:40 -0400

    Sieve of Eratosthenes

---
+++ programming_praxis/sieve_of_eratosthenes.c
@@ -0,0 +1,152 @@
+/******************************************************************************
+ *  Sieve of Eratosthenes                                                     *
+ *  16 August 2011                                                            *
+ *                                                                            *
+ *  The Sieve of Eratosthenes starts by making a list of all the numbers up to*
+ *  a desired maximum; we'll illustrate the method by calculating the prime   *
+ *  numbers through thirty:                                                   *
+ *  2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28  *
+ *  29 30                                                                     *
+ *  Now take the first number of the list, 2, and cross off every second num- *
+ *  ber:                                                                      *
+ *  2 3 5 7 9 11 13 15 17 19 21 23 25 27 29                                   *
+ *  Next, take the next number on the list that isn't crossed off, and cross  *
+ *  off every third number; some of them have already been crossed off:       *
+ *  2 3 5 7 11 13 17 19 23 25 29                                              *
+ *  Repeat that last step for the next un-crossed number on the list, 5:      *
+ *  2 3 5 7 11 13 17 19 23 29                                                 *
+ *                                                                            *
+ *  This method is called a sieve because it sweeps through a range of num-   *
+ *  bers, with each prime number, as it is discovered, blocking all its multi-*
+ *  ples from falling through as prime numbers. The sieve admits several opti-*
+ *  mizations. First, only odd numbers are considered, since the initial      *
+ *  shifting crosses off all the even numbers except 2, which is handled      *
+ *  seperately. Second, crossing off starts at the square of the number being *
+ *  sifted, since all smaller primes have been crossed off by previous steps  *
+ *  of the sieve; for instance, sifting by 3 starts at 9 since 6 was crossed  *
+ *  off when sifting by 2. Third, sifting stops at the square root of the     *
+ *  maximum number in the sieve, since any non-primes larger than the square  *
+ *  root must have already been crossed off at previous levels of the sieve;  *
+ *  thus, in the above example there is no need to sieve on the prime number  *
+ *  7, or any larger prime number, since the square of 7 is greater than 30,  *
+ *  which is the largest number in the list.                                  *
+ *                                                                            *
+ *  Write a function that takes a single argument n and returns a list of     *
+ *  prime numbers less than or equal to n using the optimized sieve algorithm *
+ *  described above. Apply the function to the argument 15485863 and count the*
+ *  number of primes returned.                                                *
+ *****************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+/******************************************************************************
+ *  Iterates through the list and crosses off (set to -1) multiples of x      *
+ *  @param long* list                                                         *
+ *  @param long length                                                        *
+ *  @param long start - Where to start in the array (code adds 1).            *
+ *  @param long num   - Value of the current location (x).                    *
+ *  @return long      - Number of values crossed off.                         *
+ *****************************************************************************/
+long cross_off_list(long* list, long length, long start, long num) {
+    long i = 0;
+    long j = 0;
+    for(i = start+1; i < length; i++) {
+        if(list[i] % num == 0) {
+            list[i] = -1;
+            j++;
+        }
+    }
+
+    return j;
+}
+
+void print_list(long *list, long length) {
+    long i = 0;
+    for(i = 0; i < length; i++) {
+        printf("%ld ", list[i]);
+    }
+    printf("\n");
+}
+
+/*****************************************************************************
+ *  Create a new list, copy all valid entries over.                          *
+ *  @param long* list                                                        *
+ *  @param long size                                                         *
+ *  @note malloc on the new list.                                            *
+ *  @note free on the old list.                                              *
+ *  @return pointer to the new list.                                         *
+ ****************************************************************************/
+long* create_new_list(long* list, long size) {
+    long* new_list = (long *)malloc(size*sizeof(long));
+    long i         = 0;
+    for(i = 0; i < size; i++) {
+        if(list[i] == -1) { break; }
+        new_list[i] = list[i];
+    }
+    
+    free(list);
+
+    return new_list;
+}
+
+/******************************************************************************
+ *  Shift removed values (-1) to the end of the list.                         *
+ *  @param long* list                                                         *
+ *  @param long length                                                        *
+ *****************************************************************************/
+void shift_list(long* list, long length) {
+    long i = 0;
+    long j = 0;
+    long t = 0;
+
+    while(i < length) {
+        if(list[i] == -1) {
+            for(j = i; j < length-1; j++) {
+                t         = list[j];
+                list[j]   = list[j+1];
+                list[j+1] = t;
+            }
+        }
+        i++;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    long maximum, size;
+    long stop,i,j;
+    long num_removed;
+    long* list;
+
+    if(argc == 2) {
+        maximum = atol(argv[1]);    // Upper bound
+        size    = maximum-1;        // Ignore 0,1
+
+        // Fill list
+        list = (long *) malloc(size*sizeof(long));
+        for(i = 0; i < size; i++) {
+            list[i] = i+2;
+        }
+
+        // Run sieve algorithm.
+        i = 0;
+        stop = floor(sqrt(maximum));
+        while(i <= stop) {
+            num_removed = cross_off_list(list, size, i, (i+2));
+            shift_list(list, size);
+            size -= num_removed;
+
+            list = create_new_list(list,size);           
+            i++;
+        }
+
+        // Final output
+        print_list(list,size);
+        printf("Count: %ld\n", size);
+    }else {
+        printf("./%s [maximum]\n",argv[1]);
+    }
+    
+    return 0;
+}
