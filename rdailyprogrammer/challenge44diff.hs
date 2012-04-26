af9f9139a9ad7608e64615e6424d3f795af19d81
Thomas Shaffer <tjshaffer21@gmail.com>
2012-04-26 17:51:02 -0500

    Implemented Miller Rabin; currently slow for large test cases.

--- rdailyprogrammer/challenge44diff.hs
+++ rdailyprogrammer/challenge44diff.hs
@@ -16,14 +16,15 @@
  - TODO Implement more efficent algorithm.
  -}
 import System.Random
+import Control.Parallel
 
 sieve :: Int -> Int -> [Int]
-sieve min max = [x | x <- 2:primesN [y | y <- [3..max], odd y], x >= min]
+sieve min max = [x | x <- [min..max], odd x, millerRabin x 1]
 
 -- Naive implementation
-primesN :: [Int] -> [Int]
-primesN []       = []
-primesN (x:xs)   = x:(primesN (dropMult x xs))
+naive :: [Int] -> [Int]
+naive []       = []
+naive (x:xs)   = x:(naive (dropMult x xs))
 
 dropMult :: Int -> [Int] -> [Int]
 dropMult n []       = []
@@ -32,15 +33,62 @@ dropMult n (x:xs)
     | mod x n == 0  = dropMult n xs
     | otherwise     = x:dropMult n xs
 
+-- Miller-Rabin
+-- Input:   n > 3, an odd integer to be tested for primality
+-- Intput:  k, a parameter that determines the accuracy of the test.
+millerRabin :: Int -> Int -> Bool 
+millerRabin n k
+    | n < 0          = error "Invalid integer."
+    | n < 3 && n > 1 = True
+    | k > 0     = let a = head . head $ take 1 (rand (mkStdGen 100) 2 (n - 2))
+                      ds = findDS (n-1)
+                      d  = fst ds
+                      x :: Integer
+                      x  = mod ((fromIntegral a) ^ d) (fromIntegral n) 
+                  in if x == 1 || x == ((fromIntegral n) - 1) then
+                        millerRabin n (k - 1)
+                     else
+                        mrLoop (fromIntegral x) n k ((snd ds)-1)
+    | otherwise = True
+
+-- x, a^d % n
+-- n, Test number
+-- k, Accuracy
+-- s, From 2^d * s
+mrLoop :: Int -> Int -> Int -> Int -> Bool
+mrLoop x n k s
+    | s > 0     = let xx = mod (x * x) n
+                  in if xx == 1 then
+                        False
+                     else if xx == (n - 1) then
+                        millerRabin n (k - 1)
+                     else
+                        mrLoop xx n k (s - 1)
+    | otherwise = False
+
+-- Find values for (d,s)
+findDS :: Int -> (Int, Int)
+findDS d
+    | mod d 2 == 0 = let ds = findDS (div d 2) in (fst ds,(snd ds) + 1)
+    | otherwise    = (d, 0)
+
+-- Produce random numbers [min,max]
+rand :: RandomGen a => a -> Int -> Int -> [[Int]]
+rand g min max = randomW (randomRs (min, max) g) max
+
+randomW :: [Int] -> Int -> [[Int]]
+randomW (r:rs) max = let (xs,ys) = splitAt max rs in xs : randomW ys max
+
 factorial :: Int -> Int
 factorial 1 = 1
-factorial n = n * factorial (n-1)
+factorial n
+    | () `seq` n `seq` False = undefined
+    | n < 1     = error "Invalid number."
+    | otherwise = n * factorial (n-1)
 
-main = do
-    let a = factorial 16
-    let b = factorial 10
-    let s = sieve a (a + b)
-
-    putStrLn $ "There are " ++ (show $ length s) ++ " primes between " ++ 
-        (show a) ++ " and " ++ (show $ a + b) ++ " and the sum is " ++ 
-        (show $ sum s)
+main = do a `pseq` c `pseq` (s `pseq` putStrLn $ "There are " ++ 
+            (show $ length s) ++ " primes between " ++ (show a) ++ " and " 
+            ++ (show $ c) ++ " and the sum is " ++ (show $ sum s))
+          where a = factorial 9--factorial $! 16
+                c = a + factorial 8--factorial $! 10
+                s = sieve a (c)
